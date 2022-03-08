8625c99ba113bb7730f36d66e86ce0e2d355c887
Fenghe Xu <jxphxufh@gmail.com>
2022-03-08 16:22:12 +0000

    refactor: move server into a namespace

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -28,7 +28,7 @@ void fileReadLocalTestUtil() {
 
 int main() {
     
-    FileServer server(5000, "/home/fenghe/contests/jk/client-server/common/data");
+    file_server::FileServer server(5000, "/home/fenghe/contests/jk/client-server/common/data");
     
     return 0;
 }
