c6b62c834684c8a51fa8c5ece2f2ff84846442a0
Fenghe Xu <jxphxufh@gmail.com>
2022-03-13 13:20:02 +0000

    fix: 5 server instances

--- start-server.sh
+++ start-server.sh
@@ -6,3 +6,5 @@ mkdir output/
 nohup server/build/file_server 40017 /data/team-10/remote/ > output/server1.out &
 nohup server/build/file_server 40018 /data/team-10/remote/ > output/server2.out &
 nohup server/build/file_server 40019 /data/team-10/remote/ > output/server3.out &
+nohup server/build/file_server 40020 /data/team-10/remote/ > output/server4.out &
+nohup server/build/file_server 40021 /data/team-10/remote/ > output/server5.out &
