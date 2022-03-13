507a590da92dec025dc37606735d3d19bf37b9d5
Fenghe Xu <jxphxufh@gmail.com>
2022-03-13 12:32:50 +0000

    fix: client worker id

--- start-client.sh
+++ start-client.sh
@@ -3,6 +3,6 @@ rm -rf output/
 mkdir output/
 
 nohup client/build/file_client 0 40017 10.216.68.190 12345 /data/team-10/multiprocess-remote-test-limit/  > output/client1.out &
-nohup client/build/file_client 0 40018 10.216.68.190 12344 /data/team-10/multiprocess-remote-test-limit/  > output/client2.out &
-nohup client/build/file_client 0 40019 10.216.68.190 12343 /data/team-10/multiprocess-remote-test-limit/  > output/client3.out &
+nohup client/build/file_client 1 40018 10.216.68.190 12344 /data/team-10/multiprocess-remote-test-limit/  > output/client2.out &
+nohup client/build/file_client 2 40019 10.216.68.190 12343 /data/team-10/multiprocess-remote-test-limit/  > output/client3.out &
 
